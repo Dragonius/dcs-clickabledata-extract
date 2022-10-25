6002d500317c936aff5b0ed59d83656c12045a1e
Jari Luukkonen <Dragonius@users.noreply.github.com>
2022-10-25 18:28:29 +0300

    Update extract_module_main.lua
    Generates all cockpits key bindings

--- extract_module_main.lua
+++ extract_module_main.lua
@@ -1,8 +1,12 @@
-local all_planes={"A-10C", "A-10C_2", "AJS37", "AH-64D", "AV8BNA", "Bf-109K-4", "C-101CC", "C-101EB", "Christen Eagle II", "F-16C", "F-5E", "F-86", "F14", "FA-18C", "FW-190A8", "FW-190D9", "I-16", "JF-17", "Ka-50", "L-39C", "L-39ZA", "M-2000C", "MIG-21bis", "Mi-8MTV2", "MiG-15bis", "MiG-19P", "SA342", "SpitfireLFMkIX", "Su-25T", "Su-33", "TF-51D", "Uh-1H", "Yak-52"}
+local all_planes={"A-10C", "A-10C_2", "AH-64D", "AJS37", "AV8BNA", "Bf-109K-4", "C-101CC", "C-101EB", "Christen Eagle II", "F-16C", "F-5E", "F-86", "F14", "FA-18C", "FW-190A8", "FW-190D9", "I-16", "JF-17", "Ka-50", "L-39C", "L-39ZA", "M-2000C", "MIG-21bis", "Mi-24P", "Mi-8MTV2", "MiG-15bis", "MiG-19P", "SA342", "SpitfireLFMkIX", "Su-25T", "Su-33", "TF-51D", "Uh-1H", "Yak-52"}
 
---Not working P-51D submodel  P-51D-25-NA
+--Not working P-51D submodel  P-51D-25-NA -> Crashes
 --Not working AV8BNA, Wrong location clickabledata.lua ->  \Cockpit\
-
+--Not working F14, Wrong location clickabledata.lua ->  \Cockpit\
+--Not working M-21bis, Wrong location clickabledata.lua ->  \Cockpit\
+--Not working SA342, Wrong location clickabledata.lua ->  \Cockpit\
+--Not working Su-25T, no clickabledata.lua
+--Not working Su-33, no clickabledata.lua
 
 local onlist = 1
 local size1 = 0
